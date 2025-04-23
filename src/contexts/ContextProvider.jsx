6b0fcb46cbf4cd406b98aab0966e41b01d66081e
YashParmar00 <yashsingh0033@gmail.com>
2025-04-23 17:27:30 +0530

    Charts and Navbar Functionallity Added

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable react-refresh/only-export-components */
-import React, { createContext, useContext, useState } from "react";
-import { userProfileData } from "../data/dummy";
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
@@ -12,18 +10,30 @@ const initialState = {
 };
 
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setactiveMenu] = useState(true);
-  const [isClicked, setisClicked] = useState(initialState);
-  const [screenSize, setscreenSize] = useState(undefined);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [themeSettings, setThemeSettings] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
 
-  const handleClick = (clicked) => {
-      setisClicked({ ...initialState, [clicked]: true });
-  }
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
+    setThemeSettings(false);
+  };
+
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem('colorMode', color);
+    setThemeSettings(false);
+  };
+
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
 
   return (
-    <StateContext.Provider
-      value={{ activeMenu, setactiveMenu, isClicked, setisClicked, handleClick, screenSize, setscreenSize }}
-    >
+  
+    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
       {children}
     </StateContext.Provider>
   );
